87d4e0da44a3f414c8e5ed758f50bfb4693d4efa
Jane Pong <officeofjane@gmail.com>
2017-06-09 11:23:37 +0800

    add master image

--- config/article.js
+++ config/article.js
@@ -9,7 +9,7 @@ export default () => ({ // eslint-disable-line
 
   // To set an exact publish date do this:
   //       new Date('2016-05-17T17:11:22Z')
-  publishedDate: new Date(),
+  publishedDate: new Date('2017-06-12T05:00:00Z'),
 
   headline: 'North Korea missiles',
 
@@ -33,7 +33,7 @@ export default () => ({ // eslint-disable-line
     title: '',
     description: '',
     credit: '',
-    url: 'https://image.webservices.ft.com/v1/images/raw/http%3A%2F%2Fcom.ft.imagepublish.prod.s3.amazonaws.com%2Fc4bf0be4-7c15-11e4-a7b8-00144feabdc0?source=ig&fit=scale-down&width=700',
+    url: './images/NKMissilesMas.jpg',
     width: 2048, // ensure correct width
     height: 1152, // ensure correct height
   },
