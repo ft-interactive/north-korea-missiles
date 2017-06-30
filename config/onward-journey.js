82d87bdf42032a029fb8d843435cc73fb21a55b6
Martin Stabe <martinstabe@gmail.com>
2017-06-30 10:42:57 +0100

    Focus OJ on NK

--- config/onward-journey.js
+++ config/onward-journey.js
@@ -1,7 +1,6 @@
 export default () => ({
   relatedContent: [
     // { rows: 1, list: 'list/graphics' },
-    { rows: 1, list: 'thing/TnN0ZWluX0dMX0tQ-R0w=' },
-    { rows: 1, list: 'thing/MTcw-U2VjdGlvbnM=' },
+    { rows: 2, list: 'thing/TnN0ZWluX0dMX0tQ-R0w=' },
   ],
 });
