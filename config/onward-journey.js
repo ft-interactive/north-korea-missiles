d8394252fb5b5667b79cfdf3f6536b78e5ec186c
Callum Locke <callum.locke@gmail.com>
2017-08-01 19:00:57 +0100

    switch to new style UUID for onward journey

--- config/onward-journey.js
+++ config/onward-journey.js
@@ -1,6 +1,6 @@
 export default () => ({
   relatedContent: [
     // { rows: 1, list: 'list/graphics' },
-    { rows: 2, list: 'thing/TnN0ZWluX0dMX0tQ-R0w=' },
+    { rows: 2, list: 'thing/8a218cba-b242-3d33-b730-d3fb2f7a258b' },
   ],
 });
