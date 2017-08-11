4a2b7383792f5bb01dafe27e5d1bc5852e9763d5
Martin Stabe <martinstabe@gmail.com>
2017-08-11 11:02:40 +0100

    Update OJ to North Korea

--- config/onward-journey.js
+++ config/onward-journey.js
@@ -1,6 +1,6 @@
 export default () => ({
   relatedContent: [
     // { rows: 1, list: 'list/graphics' },
-    { rows: 2, list: 'thing/8a218cba-b242-3d33-b730-d3fb2f7a258b' },
+    { rows: 2, list: 'thing/d67cef02-e3ee-3260-8ac1-9c67d7a1263c' },
   ],
 });
